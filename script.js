2c3eba418933cc2042a9ab098603735324e2e4b0
Alexander Peil <you@example.com>
2023-01-12 13:13:08 +0100

    installed google fonts

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ let currentPokemon;
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let response = await fetch(url);
-    let currentPokemon = await response.json();
+    currentPokemon = await response.json();
 
-    console.log('current Pokemon is:', responseAsJson);
+    console.log('current Pokemon is:', currentPokemon);
+
+    renderPokemonInfo();
+}
+
+
+function renderPokemonInfo() {
+    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemon-img').src = currentPokemon['sprites']['front_shiny'];
 }
