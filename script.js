8981f1445b4fb41968e81710ac0776837b4df364
Alexander Peil <you@example.com>
2023-01-13 12:56:40 +0100

    added pokemon API and declared some functions)

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
+let allPokemon = [];
 let currentPokemon;
+let pokemonName;
+let pokemonImage;
+let pokemonType;
+let pokemonColor;
+let limit = 50;
 
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('current Pokemon is:', currentPokemon);
-
-    renderPokemonInfo();
+    for (let i = 0; i < limit; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        allPokemon.push[currentPokemon];
+    }
+    pokemonInfo();
 }
 
 
-function renderPokemonInfo() {
-    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemon-img').src = currentPokemon['sprites']['front_shiny'];
+function pokemonInfo() {
+    let container = document.getElementById('main-container');
+    container.innerHTML = '';
+    for (let i = 0; i < allPokemon.length; i++) {
+        const pokemon = allPokemon[i];
+
+        pokemonName = pokemon['name'];
+        pokemonImage = pokemon[`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i++}.svg`];
+        pokemonType = pokemon['types'][0]['type']['name'];
+        container.innerHTML += renderPokemonInfo(pokemonName, pokemonImage, pokemonType);
+    }
+}
+
+function renderPokemonInfo(pokemonName, pokemonImage, pokemonType) {
+    return /*html*/ `
+    <div id="pokedex-card">
+         <div>
+             <h2>${pokemonName}</h2>
+         </div>
+         <div>
+             <img src="${pokemonImage}">
+         </div>
+         <div>
+            <h2>${pokemonType}</h2>
+         </div>
+    </div>
+    `;
 }
